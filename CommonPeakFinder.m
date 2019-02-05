8a1652b60762ca0bc47bf19dd381ccaa26dbd926
betts-jt <01betjos@gmail.com>
2019-02-05 16:53:58 +0000

    adjust to function correctly and remove the need to have peaks in the correct order

--- CommonPeakFinder.m
+++ CommonPeakFinder.m
@@ -5,23 +5,20 @@ function [FreqCommonPeaks] = CommonPeakFinder(freqPeaks1, freqPeaks2)
 %   freqPeaks1 = The frequencies of peaks in part 1
 %   freqPeaks2 = The frequencies of peaks in part 2
 
-ComparePeaks = freqPeaks1 - freqPeaks2; % Difference between the peaks for the first and second part.
-CommonPeaks  = zeros(1,length(ComparePeaks)); % Initiallise variable to cound common peaks
+tol = 15; % The difference that is allowed within the parts frtequency peaks for them to be concidered the same
 
-tol = 5; % The difference that is allowed within the parts frtequency peaks for them to be concidered the same
-
-for i = 1:length(ComparePeaks)
-    if abs(ComparePeaks(i)) < tol % There is a common peak within the parts
-        CommonPeaks(i) = 1;
-    else % There isn't a common peak
-        CommonPeaks(i) = 0;
+%Check if the peaks from part 1 are found in part 2
+for i = 1:length(freqPeaks1)
+    for k = 1:length(freqPeaks2)
+        Common(k,i) = freqPeaks2(k)-freqPeaks1(i);
     end
+    PeakFound(i) = any(abs(Common(:,i))<tol);
 end
 
 %Find the frequency of common peaks
 j=1; % Counter for setting FreqCommonPeaks correctly
-for i = 1:length(CommonPeaks)
-    if CommonPeaks(i) == 1 % If there is a common peak
+for i = 1:length(freqPeaks1)
+    if PeakFound(i) == 1 % If there is a common peak
         FreqCommonPeaks(j) = freqPeaks1(i); % add the frequency to the common frequency variable.
         j = j+1;
     end
