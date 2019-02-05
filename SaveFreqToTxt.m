69a81f2c6b47907cade7f1ed37cdaea7a4fae1a2
betts-jt <01betjos@gmail.com>
2019-02-05 15:38:51 +0000

    Generate code to save the common frequencies

---
+++ SaveFreqToTxt.m
@@ -0,0 +1,22 @@
+function [] = SaveFreqToTxt(FreqCommonPeaks)
+% This function saves the common frequecies to file to allow then to be
+% read later.
+%   Where
+%   FreqCommonPeaks is an array of freqencies common between two test parts
+
+% prompt user to enter name to same file as
+fileName = input('Please insert a file name to save the common frequencies as \n Leave black if you do not want to save. \n', 's');
+
+% Check if fileName was left blakc and therefore the user doesn't want to
+% save
+
+if isempty(fileName)
+    return
+end
+
+% Save the frequencies as a Mablad data file with the input name
+save(fileName, FreqCommonPeaks)
+
+end
+
+
