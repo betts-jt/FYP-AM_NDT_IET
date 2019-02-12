66cf02f9c27d1cf669c83ae4b77738fce9f366a2
betts-jt <01betjos@gmail.com>
2019-02-12 16:35:46 +0000

    Update FrequencyComparrison.m

--- FrequencyComparrison.m
+++ FrequencyComparrison.m
@@ -5,7 +5,6 @@ close all
 % This script allows for the easy comparrison of part freqency graphs.
 
 AudioRequired = 1; % Set variable to check if the user wishes to enter audio
-LoopCount = 0; % Counter for the number of audio clips analysed
 GoodCount = 0; % Counter for the numebr of good parts that have been analysed
 BadCount = 0; % Counter for the numebr of good parts that have been analysed
 
@@ -14,7 +13,6 @@ hold on
 grid on
 
 while AudioRequired == 1
-    LoopCount = LoopCount + 1; % Add one to counter
     
     % Check if the user withes to enter a known good or bad part
     answer = questdlg('What type of audio do you want to be analysed?','Audio Selection','Good Part', 'Bad Part', 'Finish Input', 'Finish Input');
@@ -45,15 +43,49 @@ while AudioRequired == 1
             
             [XB(:,BadCount), fB(:,BadCount), a, b] = Time_Freq_domain([answer{1}, '.wav']);
             
-            
             semilogx(fB(:,BadCount), XB(:,BadCount), 'r') % Plot known good part sprectrum
             
             freqPeaksBad(:,BadCount) = fB(b,BadCount);
             
         case 'Finish Input'
             AudioRequired = 0; % End audio input loop
-            
     end
-    
 end
 
+GoodPeaks = CommonPeakFinder(freqPeaksGood); % Find the good part peaks
+BadPeaks = CommonPeakFinder(freqPeaksBad); % Find the bad part peaks
+
+% Check if any of the good part peaks are on the bad peak list
+tol = 100; % tolerence of difference in peak value to be concidered the same
+
+for i = 1: length(GoodPeaks)
+    counter = 0;
+   for k = 1:length(BadPeaks)
+       counter = counter+1;
+       difference = abs(BadPeaks(counter)-GoodPeaks(i)); % Calculate the difference between the badPeaks and GoodPeaks value
+       if difference < tol % If the value if lower than the tol value
+           BadPeaks(counter) = []; % remove the bad peak the is the same as the good peak
+           counter = counter - 1;
+       elseif BadPeaks(counter) == 0 % If the value of badpeaks is zero
+           BadPeaks(counter) = []; % remove the bad peak the is the same as the good peak
+           counter = counter - 1;
+       end
+   end
+end
+
+
+% Plot the peak lines on the graph
+% Good part peak lines
+for i = 1:length(GoodPeaks)
+    xline(GoodPeaks(i), 'k') % Plot the peak value line
+    xline(GoodPeaks(i)-tol, '--k') % Plot the lower tolerence peak line
+    xline(GoodPeaks(i)+tol, '--k') % Plot the lower tolerence peak line
+end
+% Bad part peak lines
+for i = 1:length(BadPeaks)
+    xline(BadPeaks(i), 'r') % Plot the peak value line
+    xline(BadPeaks(i)-tol, '--r') % Plot the lower tolerence peak line
+    xline(BadPeaks(i)+tol, '--r') % Plot the lower tolerence peak line
+end 
+    
+    
