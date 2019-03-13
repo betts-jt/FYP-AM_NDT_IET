e61f8681c5ecbd052176e52dc528a387a2460a0a
betts-jt <01betjos@gmail.com>
2019-03-13 16:28:32 +0000

    Edit to use FFT example

--- Time_Freq_domain_Frequency.m
+++ Time_Freq_domain_Frequency.m
@@ -5,19 +5,27 @@ x = x(:, 1);                        % get the first channel
 RequiredData = length(x);                      % signal length
 t = (0:RequiredData-1)/fs;                     % time vector
 
+%{
 % spectral analysis
 w = hanning(RequiredData, 'periodic');
 [X, f] = periodogram(x, w, RequiredData, fs, 'power');
 X = 20*log10(sqrt(X)*sqrt(2));
+%}
+
+% Experimental analysis
+NFFT = 2^nextpow2(RequiredData); % Next power of 2 from length of y
+Y = fft(x,NFFT)/RequiredData;
+f = fs/2*linspace(0,1,NFFT/2+1);
+X = 2*abs(Y(1:NFFT/2+1));
+
 
 %  Plot largest peaks on first part
-minPeakProminence = 25; % The minimum peak provinence for finding peaks
+minPeakProminence = 0.5; % The minimum peak provinence for finding peaks
 NumPeaks = 100; % Reset the value of number of peaks
 
 RequiredPeaks = 15; % Set required nuber of peaks
 
-[a, b, ~, PeakProm] = findpeaks(X, 'MinPeakProminence', minPeakProminence);
-minPeakProminence = minPeakProminence+1;
+[a, b, ~, PeakProm] = findpeaks(X);
 NumPeaks = length(a);
 
 data(:,1) = a; % Set column one to the peak magnitude values
