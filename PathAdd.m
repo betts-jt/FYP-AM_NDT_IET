4009436c8851906581ee5e13645a3d7c43a5b714
betts-jt <01betjos@gmail.com>
2019-03-13 12:56:41 +0000

    update

--- PathAdd.m
+++ PathAdd.m
@@ -2,6 +2,6 @@ function [] = Pathadd()
 % This function adds all the nessusary paths to file for the testing
 % functions to work.
 
-addpath 'Audio_Clips' 'Data' 'Data/ErrorFreq' 'Data/CommonFreq' 'Figures'
+addpath 'Audio_Clips' 'Data' 'Data/ErrorFreq' 'Data/CommonFreq' 'Figures' 'Audio_Clips/Training_Data' 'Audio_Clips/Training_Data/Good' 'Audio_Clips/Training_Data/Bad'
 
 end
