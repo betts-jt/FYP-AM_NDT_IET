9d00c910d064beae8187c6696ded0c128a8f08e4
betts-jt <01betjos@gmail.com>
2019-03-13 14:12:17 +0000

    Add correct path

--- Multiple_Audio_Record.m
+++ Multiple_Audio_Record.m
@@ -3,7 +3,7 @@ close all
 
 BaseName = '0_0_'; % The begining of the file name.
 
-Path = 'C:\Users\joebe\iCloudDrive\Documents\University\Year 4\FYP\FYP-AM_NDT_IET\Audio_Clips\Training_Data\Good\';
+Path = 'E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\Good\';
 
 for i=1:30
 
