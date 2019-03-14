5dca7f339222c409cc491dd82eca19fb7f5f1599
betts-jt <01betjos@gmail.com>
2019-03-14 18:23:54 +0000

    add more good training data

--- Multiple_Audio_Record.m
+++ Multiple_Audio_Record.m
@@ -5,7 +5,7 @@ BaseName = '0_0_'; % The begining of the file name.
 
 Path = 'E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\Good\';
 
-for i=1:30
+for i=31:60
 
 RecordAudio(Path, [BaseName num2str(i)], 1.5)
 
