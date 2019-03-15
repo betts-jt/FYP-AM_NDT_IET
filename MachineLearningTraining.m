4ded344536c0a517adaeaac5b16e959f4d44181f
betts-jt <01betjos@gmail.com>
2019-03-15 17:32:09 +0000

    Edit to make the number of neurons smaller

--- MachineLearningTraining.m
+++ MachineLearningTraining.m
@@ -5,11 +5,11 @@ OutData = CSVimport(Path); % Run the funciton to import and format the data
 [x, t] = DataProcessing(OutData); % Format the data for the neural network
 
 %create a nural network layout for the problem
-net = patternnet([1024 1024]); % creates a two hidden layer neural network with 1024 hidden neurons in each layer
+net = patternnet([512 512]); % creates a two hidden layer neural network with 1024 hidden neurons in each layer
 view(net) % dispalys the layout of the neural network
 
 % Train the neural network based on the above layout and data
-[net,tr] = train(net,x,t, 'UseParallel','yes','UseGPU','yes');
+[net,tr] = train(net,x,t);
 
 %Plot networks performance
 plotperform(tr)
