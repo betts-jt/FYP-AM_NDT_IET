4898d773ff718aadd2d7c1b04e78cef999bbd333
betts-jt <01betjos@gmail.com>
2019-03-15 15:14:11 +0000

    Edit to correctly read thorugh a list of audiofiles

--- Multitrack_Read.m
+++ Multitrack_Read.m
@@ -1,14 +1,16 @@
 % This script runs through all of the audio files with the same initial
 % file make and writes the results to a csv file.
 
-PreName = '0_0_'; % Initial name of the audio file
-Path = 'E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\Good\'; % Path where the audio files are located
-NumFiles = 90; % Number of audio files of that type
-OutName = 'test'; % Output ifle name for the csv
-Category = 0; % Category for the data (0 = good, 1 = bad, ect)
+clear
 
-for i = 1:90
-    [x, fs] = audioread([Path PreName num2str(i) '.wav']);   % load an audio file
+PreName = '0_50_'; % Initial name of the audio file
+InPath = 'E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\Initial_Test_Cylinder\Bad\50\'; % Path where the audio files are located
+NumFiles = 30; % Number of audio files of that type
+OutName = 'Bad50'; % Output ifle name for the csv
+Category = 1; % Category for the data (0 = good, 1 = bad, ect)
+
+for i = 1:NumFiles
+    [x, fs] = audioread([InPath PreName num2str(i) '.wav']);   % load an audio file
     x = x(:, 1); % get the first channel
     RequiredData = length(x); % signal length
     t = (0:RequiredData-1)/fs; % time vector  
@@ -22,4 +24,4 @@ end
 
 a(1:NumFiles,:) = X;
 a(:,length(X)+1) = Category;
-csvwrite([Path OutName '.csv'], a')
+csvwrite(['E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\TestCSV\' OutName '.csv'], a')
