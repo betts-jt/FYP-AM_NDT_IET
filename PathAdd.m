cfd63f8ebdf16a192a2d9a716ddd493c49a25145
betts-jt <01betjos@gmail.com>
2019-03-15 15:09:28 +0000

    Update to include new folders

--- PathAdd.m
+++ PathAdd.m
@@ -2,6 +2,7 @@ function [] = Pathadd()
 % This function adds all the nessusary paths to file for the testing
 % functions to work.
 
-addpath 'Audio_Clips' 'Data' 'Data/ErrorFreq' 'Data/CommonFreq' 'Figures' 'Audio_Clips/Training_Data' 'Audio_Clips/Training_Data/Good' 'Audio_Clips/Training_Data/Bad'
-
+addpath 'Audio_Clips' 'Data' 'Data/ErrorFreq' 'Data/CommonFreq' 'Figures'
+addpath(genpath('Audio_Clips'))
+addpath(genpath('TestCSV'))
 end
