55b982b7aeafc533939a02a509ce3cd190140d29
betts-jt <01betjos@gmail.com>
2019-03-15 14:57:00 +0000

    Adjust to read audi correctly

--- Multitrack_Read.m
+++ Multitrack_Read.m
@@ -1,8 +1,18 @@
+% This script runs through all of the audio files with the same initial
+% file make and writes the results to a csv file.
+
+PreName = '0_0_'; % Initial name of the audio file
+Path = 'E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\Good\'; % Path where the audio files are located
+NumFiles = 90; % Number of audio files of that type
+OutName = 'test'; % Output ifle name for the csv
+Category = 0; % Category for the data (0 = good, 1 = bad, ect)
+
 for i = 1:90
-    [x, fs] = audioread(['E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\Good\0_0_' num2str(i) '.wav']);   % load an audio file
-    x = x(:, 1);                        % get the first channel
-    RequiredData = length(x);                      % signal length
-    t = (0:RequiredData-1)/fs;                     % time vector   
+    [x, fs] = audioread([Path PreName num2str(i) '.wav']);   % load an audio file
+    x = x(:, 1); % get the first channel
+    RequiredData = length(x); % signal length
+    t = (0:RequiredData-1)/fs; % time vector  
+    
     % Experimental analysis
     NFFT = 2^nextpow2(RequiredData); % Next power of 2 from length of y
     Y = fft(x,NFFT)/RequiredData;
@@ -10,7 +20,6 @@ for i = 1:90
     X(i,:) = 2*abs(Y(1:NFFT/2+1));
 end
 
-a(1,1:65537) = f;
-a(2:91,:) = X;
-a(:,65538) = 0;
-%csvwrite('Good2.csv', a')
+a(1:NumFiles,:) = X;
+a(:,length(X)+1) = Category;
+csvwrite([Path OutName '.csv'], a')
