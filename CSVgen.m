f51b1ecc7e9756a346756ef3823320e94081789d
betts-jt <01betjos@gmail.com>
2019-03-16 01:02:36 +0000

    Update CSVgen.m

--- CSVgen.m
+++ CSVgen.m
@@ -3,11 +3,11 @@
 
 clear
 
-PreName = '0_50_'; % Initial name of the audio file
-InPath = 'E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\Initial_Test_Cylinder\Bad\50\'; % Path where the audio files are located
-NumFiles = 30; % Number of audio files of that type
-OutName = 'Bad50'; % Output ifle name for the csv
-Category = 1; % Category for the data (0 = good, 1 = bad, ect)
+PreName = '0_1_'; % Initial name of the audio file
+InPath = 'E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\Initial_Test_Cylinder\Bad\1\'; % Path where the audio files are located
+NumFiles = 28; % Number of audio files of that type
+OutName = 'Bad1'; % Output ifle name for the csv
+Category = 7; % Category for the data (0 = good, 1 = bad, ect)
 
 for i = 1:NumFiles
     [x, fs] = audioread([InPath PreName num2str(i) '.wav']);   % load an audio file
