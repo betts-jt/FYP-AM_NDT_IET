cb8eee0979495e8a1768606049cd44ddc078252a
betts-jt <01betjos@gmail.com>
2019-03-18 20:55:45 +0000

    Add 'Lib' to PathAdd

--- PathAdd.m
+++ PathAdd.m
@@ -2,7 +2,7 @@ function [] = Pathadd()
 % This function adds all the nessusary paths to file for the testing
 % functions to work.
 
-addpath 'Audio_Clips' 'Data' 'Data/ErrorFreq' 'Data/CommonFreq' 'Figures'
+addpath 'Audio_Clips' 'Data' 'Data/ErrorFreq' 'Data/CommonFreq' 'Figures' 'Lib'
 addpath(genpath('Audio_Clips'))
 addpath(genpath('TestCSV'))
 end
