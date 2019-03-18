f477e2b63aac3ef4d62c4e054f9e9745ccc8dbe3
betts-jt <01betjos@gmail.com>
2019-03-18 20:47:01 +0000

    Update Multiple_Audio_Record.m

--- Multiple_Audio_Record.m
+++ Multiple_Audio_Record.m
@@ -1,11 +1,11 @@
 clc
 close all
 
-BaseName = '2_3_50_'; % The begining of the file name.
+BaseName = '2_3_5_'; % The begining of the file name.
 
-Path = 'E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\AM_4_Cylinders\Site3\Bad\10\Part4\';
+Path = 'E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\AM_4_Cylinders\Site3\Bad\5\Part4\';
 
-for i=14:14
+for i=6:15
     
     disp('---------------------------------');
     disp(num2str(i));
