dffc9e6dbbee41ddb8acde1d386eca28a2e33872
betts-jt <01betjos@gmail.com>
2019-03-21 13:49:18 +0000

    Update Multiple_Audio_Record.m

--- Lib/Multiple_Audio_Record.m
+++ Lib/Multiple_Audio_Record.m
@@ -3,9 +3,10 @@ close all
 
 BaseName = '2_3_5_'; % The begining of the file name.
 
-Path = 'E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\AM_4_Cylinders\Site3\Bad\5\Part4\';
+%Path = 'E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\AM_4_Cylinders\Second_Test_Set\Site3\Good\';
+Path = 'E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\AM_4_Cylinders\Second_Test_Set\Site3\Bad\5\';
 
-for i=6:15
+for i=19:30
     
     disp('---------------------------------');
     disp(num2str(i));
