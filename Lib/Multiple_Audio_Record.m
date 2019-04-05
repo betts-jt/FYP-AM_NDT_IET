d082cf9c9e69cfbf3fff5997760256b593047151
betts-jt <01betjos@gmail.com>
2019-04-05 19:38:43 +0100

    Change settings for testing

--- Lib/Multiple_Audio_Record.m
+++ Lib/Multiple_Audio_Record.m
@@ -1,14 +1,14 @@
 clc
 close all
 
-Percent_Fill = 50;
+Percent_Fill = 0;
 
 BaseName = ['1_3_' num2str(Percent_Fill) '_']; % The begining of the file name.
 
 %Path = 'E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\AM_4_Cylinders\Second_Test_Set\Site3\Good\';
-Path = ['E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\Machined_Part\Site3\Bad\' num2str(Percent_Fill) '\'];
+Path = ['E:\iCloudDrive\Documents\University\Year 4\FYP\Matlab\FYP-AM_NDT_IET\Audio_Clips\Training_Data\Machined_Part\Tempurature_Testing\hot\'];
 
-for i=10:10
+for i=1:5
     
     disp('---------------------------------');
     disp(num2str(i));
